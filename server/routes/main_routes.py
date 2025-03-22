dc1cfcb27bff467fb7c95c8a9b7a7b762931e632
gabrielbaute <gabrielbaute@gmail.com>
2025-03-22 19:46:23 -0400

    Creacion de rutas para confirmar suscripcion al newsletter

--- server/routes/main_routes.py
+++ server/routes/main_routes.py
@@ -1,6 +1,7 @@
 from database import db
 from database.models import Post, ContactMessage, Subscriber
 from server.forms import ContactForm, SubscriptionForm
+from mail import send_confirmation_newsletter_email, decode_email_token
 
 import markdown
 from datetime import datetime
@@ -55,10 +56,38 @@ def contact():
 @main_bp.route('/subscribe', methods=['GET', 'POST'])
 def subscribe():
     form = SubscriptionForm()
+
     if form.validate_on_submit():
         subscriber = Subscriber(name=form.name.data, email=form.email.data)
+    
         db.session.add(subscriber)
         db.session.commit()
+
+        send_confirmation_newsletter_email(subscriber)
+    
         flash('¡Gracias por suscribirte a nuestra newsletter!', 'success')
         return redirect(url_for('main.index'))
+    
     return render_template('subscribe.html', form=form)
+
+@main_bp.route('/confirm_subscription/<token>')
+def confirm_subscription(token):
+    subscriber_id = decode_email_token(token)
+
+    if subscriber_id is None:
+        flash('El enlace de confirmación no es válido o ha expirado. Debe volver a suscribirse.', 'danger')
+        return redirect(url_for('main.subscribe'))
+    
+    subscriber = Subscriber.query.get(subscriber_id)
+
+    if subscriber.is_active:
+        flash('Ya te has suscrito, puedes seguir navegando!', 'success')
+        return redirect(url_for('main.index'))
+    
+    # Activar la suscripción del usuario
+    subscriber.is_active = True
+    db.session.commit()
+
+    flash('¡Felicidades! Gracias por suscribirte a nuestro newsletter y mantenerte atento a nuestras publicaciones.', 'success')
+    return redirect(url_for('main.index'))
+    

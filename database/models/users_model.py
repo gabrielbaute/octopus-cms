f04ec6c375664daeb08f63808f7c378ad5105d63
gabrielbaute <gabrielbaute@gmail.com>
2025-03-22 19:24:29 -0400

    Modificacion del modelo de suscriptor para permitir confirmacion de suscripcion

--- database/models/users_model.py
+++ database/models/users_model.py
@@ -6,12 +6,14 @@ class Subscriber(db.Model):
     __tablename__ = 'subscriber'
 
     id = db.Column(db.Integer, primary_key=True)
+    is_active = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     date_subscribed = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)

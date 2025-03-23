9b8b6941df2f8743adde8ff1b5368525fdf0ee14
gabrielbaute <gabrielbaute@gmail.com>
2025-03-22 20:51:54 -0400

    Formularios agregados al iniciador

--- server/forms/__init__.py
+++ server/forms/__init__.py
@@ -1,4 +1,4 @@
-from .auth_forms import LoginForm, RegistrationForm
+from .auth_forms import LoginForm, RegistrationForm, ForgotPasswordForm, ResetPasswordForm
 from .visitors_forms import ContactForm, SubscriptionForm
 from .mail_forms import NewsletterForm
 from .posts_forms import NewPostForm

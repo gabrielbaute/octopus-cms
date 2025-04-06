4f9a394055421472d7f042ede435a9993653fc54
gabrielbaute <gabrielbaute@gmail.com>
2025-04-06 11:16:34 -0400

    Handler y plantilla para error 403

--- server/errors_handler.py
+++ server/errors_handler.py
@@ -6,3 +6,7 @@ def register_error_handlers(app):
     @app.errorhandler(404)
     def page_not_found(e):
         return render_template('errors/404.html'), 404
+    
+    @app.errorhandler(403)
+    def forbidden_route(e):
+        return render_template('errors/403.html'), 404

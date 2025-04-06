366dc4ced5a49e9aae4b87ac25ca16ad628c8754
gabrielbaute <gabrielbaute@gmail.com>
2025-04-06 11:40:57 -0400

    Se unifica modelo de usuarios en una sola clase con roles y sistema de permisos

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,10 +1,11 @@
-from .users_model import Subscriber, User
+from .users_model import User, Permission, RolePermission
 from .posts_model import Post
 from .contact_message_model import ContactMessage
 
 __all__ = [
-    'subscriber',
-    'user',
-    'post',
-    'contactmessage'
+    'User',
+    'Post',
+    'ContactMessage',
+    'Permission',
+    'RolePermission'
 ]
